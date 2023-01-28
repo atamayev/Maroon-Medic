3d59961859575dc56ce1b882cd9f5ea8703e9e03
atamayev <actamayev@gmail.com>
2023-01-28 11:25:12 -0500

    adding more comments
    noticing many flaws in code writing documentation. very useful.
    --need to make crypto async
    --when logging out, will need to clear uuid cookie as well (when implemented).

--- backend/controllers/authCTRL.js
+++ backend/controllers/authCTRL.js
@@ -11,20 +11,16 @@ dotenv.config()
  *  It does this in two steps. First, it checks if the accessToken is valid (verification). If verified, the ID is extracted from the access token. The ID is then searched in the DB
  *  If there is a user's whose credentials match what was verified/queried, set verified to true. Any other case, set verified to false.
  * @param {string} req Cookie from client 
- * @param {*} res True/False
+ * @param {Boolean} res True/False
  * @returns Returns true/false, depending on wheather the cookie is verified, and if the contents of the cookie are valid
  */
 export async function jwt_verify (req, res){
-  const accessToken = req.cookies.accessToken
-  // console.log(accessToken.cookies.accessToken)
   // refer to: https://github.com/SalarC123/Classius/blob/main/src/server/router.js
   //: https://dev.to/salarc123/mern-stack-authentication-tutorial-part-1-the-backend-1c57
-    if(accessToken){
-      try{// tries to verify the token
-        console.log('test')
+      try{
+        const accessToken = req.cookies.accessToken
         const decodedDoctorID = jwt.verify(accessToken, process.env.JWT_KEY).DoctorID;
 
-        // search the doc credetials table for ids with the decoded doctor id, if exists, true, if not, false
         const table_name = 'Doctor_credentials';
         const DB_name = 'DoctorDB';
 
@@ -32,50 +28,44 @@ export async function jwt_verify (req, res){
         const values = [decodedDoctorID];
         
         await useDB(jwt_verify.name, DB_name, `${table_name}`)
+        // Searches the Doctor_credentials for the decodedDoctorID
 
-        // Queries the above SQL with values. If credentials exist in the DB
         try{const [rows] = await connection.execute(sql, values)
-          // console.log(rows)
           if (!rows.length){
-            //if there are no docs with the docID that the user is logging in with, return false
+            //If there are no doctors with the decodedDoctorID, return false
             return res.status(401).json({success: false})
           }
           else{
-            // if a doc with the provided credentials exists, return true
+            // If there is a doc with the decodedDoctorID, return true
             console.log('true')
             return res.status(200).json({success: true})
           };
         }
         catch(error){
-          // if there is a problem with the db query, return false
+          // Any problems with the query: return false
           console.log('trouble with db query', error)
           return res.status(401).json({success: false})
         }
       }
-      catch(error){// if token verification fails
-        console.log('error in token verification');
+      catch(error){
+        // If token verification fails
+        console.log('error in token verification', error);
         return res.status(401).json({success: false})
       }
   }
-    else{
-      // if there was no token provided 
-      console.log('no token provided')
-      return res.status(401).json({success: false})
-    }
-}
 
-
+/** register adds a new user's credentials to the Doctor_credentials table, and sends a 
+ *  First, register checks if the 
+ * @param {Object} req Contains the user's username, password
+ * @param {Response} res If successful, contains a cookie, and Json with user's results
+ * @returns Depending on wheather 
+ */
 // ENCRYPTION IMPLEMENTED Saves REGISTER data to the db (signs up user):
 export async function register (req, res){
     const {email, password} = req.body // Takes out the decrypted_email from the request
     const table_name = 'Doctor_credentials' // Establishes which table to query
     const DB_name = 'DoctorDB'
-    const date_ob = new Date(); // Making the DATETIME an accessible object:
-    const format = "YYYY-MM-DD HH:mm:ss"
-    const dateTime = moment(date_ob).format(format);
-    const dateTimeObj = {
-      Created_at: `${dateTime}`
-    }
+  
     const sql = `SELECT * FROM ${table_name} WHERE email = ?`;
     const values = [email];
     await useDB(register.name, DB_name, `${table_name}`)
@@ -83,6 +73,13 @@ export async function register (req, res){
     try{const [results] = await connection.execute(sql, values)
       if (results.length === 0){
         const hashed_password = await Hash.hash_credentials(password)
+        
+        const date_ob = new Date();
+        const format = "YYYY-MM-DD HH:mm:ss"
+        const dateTime = moment(date_ob).format(format);
+        const dateTimeObj = {
+          Created_at: `${dateTime}`
+        }
         const encrypted_date_time = Crypto.encrypt_single_entry(dateTimeObj).Created_at
         
         const sql = `INSERT INTO ${table_name} (email, password, Created_at) VALUES (?, ?, ?)`;
@@ -182,6 +179,11 @@ export async function login (req, res){
     res.status(500).send({ error: 'Problem with email selection' });
   }
 }
+/** logout is self-explanatory
+ *  Deletes any cookie called "accessToken"--> whenever the user navigates to future pages, their token will not be verified (token cleared)
+ * @param {n/a} req No request
+ * @param {Response} res Clears cookie, and informs that "User has been logged out"
+ */
 // Clears Cookies --> logs user out
 export async function logout (req, res){
   console.log('logged out')
