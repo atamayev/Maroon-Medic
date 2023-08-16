369d1e9a3d7a5d2224ab01365639e77599c933fc
atamayev <actamayev@gmail.com>
2023-08-15 23:53:14 -0400

    Update doctor-login-and-security.tsx

--- frontend/src/pages/private-doctor-pages/doctor-settings/doctor-login-and-security.tsx
+++ frontend/src/pages/private-doctor-pages/doctor-settings/doctor-login-and-security.tsx
@@ -1,12 +1,13 @@
+import { observer } from "mobx-react"
 import LoginHistory from "../../../components/login-history"
 import ChangePassword from "../../../components/change-password"
 import { useLoginHistory } from "../../../custom-hooks/login-history"
 import { UnauthorizedUser } from "../../../components/user-type-unauth"
 import useSimpleUserVerification from "../../../custom-hooks/use-simple-user-verification"
-import Header from "../../header"
+import Header from "../../../components/header/header"
 import DoctorHeader from "../doctor-header"
 
-export default function DoctorLoginAndSecurity() {
+const DoctorLoginAndSecurity = () => {
   const { userType } = useSimpleUserVerification()
   const loginHistory = useLoginHistory(userType, "Doctor")
 
@@ -18,11 +19,13 @@ export default function DoctorLoginAndSecurity() {
       <DoctorHeader/>
       <ChangePassword type = {userType}/>
       <h1>Login History</h1>
-      {loginHistory.map((item) => (
-        <LoginHistory key = {item.login_historyID} loginHistoryItem = {item} />
+      {loginHistory.map((item, index) => (
+        <LoginHistory key = {index} loginHistoryItem = {item} />
       ))}
       {/* Later add update history here as well. Create an update history table which has the login_history table as a foreign key.
       Within each login session, show what the user changed. */}
     </>
   )
 }
+
+export default observer(DoctorLoginAndSecurity)
