241727053c0c128bf6e1396e88709e671bbeca77
atamayev <actamayev@gmail.com>
2023-08-23 17:57:36 -0400

    Delete bootstrap, add webpack-bundle analyzer

---
+++ backend/webpack.config.js
@@ -0,0 +1,9 @@
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
+
+// Webpack configuration
+export const plugins = [
+  // other plugins...
+  new BundleAnalyzerPlugin({
+    // Options for controlling how the report looks can be added here
+  })
+]
