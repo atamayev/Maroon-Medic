69a2ff39305d0f2b9f1fea5d88760af0ade571c7
atamayev <actamayev@gmail.com>
2023-09-12 10:50:56 -0400

    Add JWT Verify to auth routes Middleware

--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -5,15 +5,16 @@ import register from "../controllers/auth/register"
 import changePassword from "../controllers/auth/change-password"
 import logout from "../controllers/auth/logout"
 import { newDoctorConfirmation, newPatientConfirmation } from "../controllers/auth/new-user-confirmation"
+import jwtVerify from "../middleware/jwt-verify"
 
 const router = express.Router()
 
 router.post("/login", login)
 router.post("/register", register)
-router.post("/logout", logout)
-router.get("/fetch-login-history", fetchLoginHistory)
-router.post("/change-password", changePassword)
-router.get("/new-doctor-confirmation", newDoctorConfirmation)
-router.get("/new-patient-confirmation", newPatientConfirmation)
+router.post("/logout", jwtVerify, logout)
+router.get("/fetch-login-history", jwtVerify, fetchLoginHistory)
+router.post("/change-password", jwtVerify, changePassword)
+router.get("/new-doctor-confirmation", jwtVerify, newDoctorConfirmation)
+router.get("/new-patient-confirmation", jwtVerify, newPatientConfirmation)
 
 export default router
