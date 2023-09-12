d7915ee37fc094018f5a0feb5ba6273d6020e46a
atamayev <actamayev@gmail.com>
2023-09-11 23:38:29 -0400

    Code Cleanup

--- frontend/src/components/patient-dashboard/personal-info.tsx
+++ frontend/src/components/patient-dashboard/personal-info.tsx
@@ -1,7 +1,12 @@
+import { useContext } from "react"
+import { AppContext } from "src/contexts/maroon-context"
 import WelcomeOrBack from "../welcome-or-back"
 
-function PersonalInfo ({ personalInfo } : {personalInfo : BirthDateInfo | null}) {
+function PersonalInfo () {
+	const { personalInfo } = useContext(AppContext)
+
 	if (!personalInfo) return <>Loading...</>
+
 	return (
 		<p>
 			Welcome <WelcomeOrBack />, {personalInfo.firstName}
