0707f0e5bd0e54af4f90cb80c6907251996a0067
atamayev <actamayev@gmail.com>
2023-10-07 14:51:22 -0400

    Remove the dashboard welcome message, code cleanup

--- frontend/src/components/welcome-or-back.tsx
+++
@@ -1,9 +0,0 @@
-import CheckCookie from "src/utils/cookie-check"
-
-export default function WelcomeOrBack () {
-	//Find another way to check if user is new. New user cookie is deleted
-	const newUser = CheckCookie.forNewUser("NewUser")
-
-	if (newUser) return <> to MaroonMedic</>
-	return <> back</>
-}
--- frontend/src/pages/patient-pages/patient-dashboard.tsx
+++ frontend/src/pages/patient-pages/patient-dashboard.tsx
@@ -9,7 +9,6 @@ import useSetPatientDashboardData from "src/custom-hooks/patient/use-set-patient
 import PatientHeader from "./patient-header"
 import UpcomingAppointmentsSection from "src/components/patient-dashboard/upcoming-appointments/upcoming-appointments-section"
 import PastAppointmentsSection from "src/components/patient-dashboard/past-appointments/past-appointments-section"
-import PersonalInfo from "src/components/patient-dashboard/personal-info"
 import AppContext from "src/contexts/maroon-context"
 
 function PatientDashboard() {
@@ -53,7 +52,6 @@ function PatientDashboard() {
 	return (
 		<div>
 			<PatientHeader />
-			<PersonalInfo />
 			<DashboardData />
 		</div>
 	)
