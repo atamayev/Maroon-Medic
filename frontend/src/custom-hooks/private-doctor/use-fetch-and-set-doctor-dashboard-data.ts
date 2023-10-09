b05f1ab74c7a0525aedbf19d08916cefc3cd34e3
atamayev <actamayev@gmail.com>
2023-10-09 16:50:38 -0400

    Dashboard code abstraction

--- frontend/src/custom-hooks/private-doctor/use-fetch-and-set-doctor-dashboard-data.ts
+++ frontend/src/custom-hooks/private-doctor/use-fetch-and-set-doctor-dashboard-data.ts
@@ -4,7 +4,7 @@ import AppContext from "src/contexts/maroon-context"
 import PrivateDoctorDataService from "src/services/private-doctor-data-service"
 import handle401AxiosError from "src/utils/handle-errors/handle-401-axios-error"
 
-export default function useSetDoctorDashboardData(): void {
+export default function useFetchAndSetDoctorDashboardData(): void {
 	const appContext = useContext(AppContext)
 
 	const fetchAndSetDashboardData: () => Promise<void> = async () => {
