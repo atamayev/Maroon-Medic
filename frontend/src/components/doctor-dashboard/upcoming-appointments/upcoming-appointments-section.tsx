d1b823bbbfd44c94638aea3a69179e90ae44e2dd
atamayev <actamayev@gmail.com>
2023-10-09 23:10:11 -0400

    Add filtering ability for upcoming appointments

--- frontend/src/components/doctor-dashboard/upcoming-appointments/upcoming-appointments-section.tsx
+++ frontend/src/components/doctor-dashboard/upcoming-appointments/upcoming-appointments-section.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import UpcomingAppointmentsMap from "./upcoming-appointments-map"
 
 interface Props {
@@ -6,14 +7,46 @@ interface Props {
 
 export default function UpcomingAppointmentsSection (props: Props) {
 	const { upcomingDoctorAppointments } = props
+	// eslint-disable-next-line max-len
+	const [filteredUpcomingDoctorAppointments, setFilteredUpcomingDoctorAppointments] = useState<DoctorDashboardData[]>(upcomingDoctorAppointments)
+	const [showApproved, setShowApproved] = useState(true)
+	const [showUnapproved, setShowUnapproved] = useState(true)
+
+	useEffect(() => {
+		let filteredAppointments: DoctorDashboardData[] = []
+
+		if (showApproved) {
+			filteredAppointments = filteredAppointments.concat(
+				upcomingDoctorAppointments.filter((appointment) => appointment.doctorConfirmationStatus)
+			)
+		}
+
+		if (showUnapproved) {
+			filteredAppointments = filteredAppointments.concat(
+				upcomingDoctorAppointments.filter((appointment) => !appointment.doctorConfirmationStatus)
+			)
+		}
+
+		setFilteredUpcomingDoctorAppointments(filteredAppointments)
+	}, [showApproved, showUnapproved, upcomingDoctorAppointments])
 
 	return (
 		<div className="mb-3 bg-yellow-100 border border-brown-400 rounded mx-3">
-			<div className="p-4 bg-amber-200 border-b border-brown-400">
+			<div className="p-4 bg-amber-200 border-b border-brown-400 flex justify-between items-center">
 				<h1 className="text-brown-800 font-bold">Upcoming Appointments</h1>
+				<div className="flex items-center">
+					<label className="mr-4">
+						<input type="checkbox" checked={showApproved} onChange={() => setShowApproved(!showApproved)} />
+						Approved
+					</label>
+					<label>
+						<input type="checkbox" checked={showUnapproved} onChange={() => setShowUnapproved(!showUnapproved)} />
+						Unapproved
+					</label>
+				</div>
 			</div>
 			<div className="p-4">
-				<UpcomingAppointmentsMap upcomingDoctorAppointments = {upcomingDoctorAppointments} />
+				<UpcomingAppointmentsMap upcomingDoctorAppointments={filteredUpcomingDoctorAppointments} />
 			</div>
 		</div>
 	)
