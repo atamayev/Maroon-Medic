cadc61e6091a0e18fffb3c7fceceb7a769f38f1f
atamayev <actamayev@gmail.com>
2023-10-14 18:06:50 -0400

    Fix personal data not loading on reload bug
    Re-formatted calling of the custom hook

--- frontend/src/custom-hooks/use-fetch-personal-info-data.ts
+++ frontend/src/custom-hooks/use-fetch-personal-info-data.ts
@@ -6,21 +6,25 @@ import PrivateDoctorDataService from "src/services/private-doctor-data-service"
 import PrivatePatientDataService from "src/services/private-patient-data-service"
 import handle401AxiosError from "src/utils/handle-errors/handle-401-axios-error"
 
-export default async function useFetchPersonalInfoData(
+export default function useFetchPersonalInfoData(): (
 	setPersonalInfo: React.Dispatch<React.SetStateAction<BirthDateInfo>>
-): Promise<void> {
+) => Promise<void> {
 	const appContext = useContext(AppContext)
 
-	try {
-		if (_.isNull(appContext.sharedData) || _.isNull(appContext.auth.userType)) return
-		let response: AxiosResponse
+	const fetchData = async (setPersonalInfo: React.Dispatch<React.SetStateAction<BirthDateInfo>>): Promise<void> => {
+		try {
+			if (_.isNull(appContext.sharedData) || _.isNull(appContext.auth.userType)) return
+			let response: AxiosResponse
 
-		if (appContext.auth.userType === "Doctor") response = await PrivateDoctorDataService.fillPersonalData()
-		else response = await PrivatePatientDataService.fillPersonalData()
+			if (appContext.auth.userType === "Doctor") response = await PrivateDoctorDataService.fillPersonalData()
+			else response = await PrivatePatientDataService.fillPersonalData()
 
-		setPersonalInfo(response.data)
-		appContext.sharedData.initializePersonalInfo(response.data)
-	} catch (error: unknown) {
-		handle401AxiosError(error)
+			setPersonalInfo(response.data)
+			appContext.sharedData.initializePersonalInfo(response.data)
+		} catch (error: unknown) {
+			handle401AxiosError(error)
+		}
 	}
+
+	return fetchData
 }
--- frontend/src/custom-hooks/use-set-personal-info.ts
+++ frontend/src/custom-hooks/use-set-personal-info.ts
@@ -6,25 +6,29 @@ import useFetchPersonalInfoData from "src/custom-hooks/use-fetch-personal-info-d
 export default function useSetPersonalInfo(
 	expectedUserType: DoctorOrPatient
 ): {
-	personalInfo: BirthDateInfo, setPersonalInfo: React.Dispatch<React.SetStateAction<BirthDateInfo>>
+	personalInfo: BirthDateInfo,
+	setPersonalInfo: React.Dispatch<React.SetStateAction<BirthDateInfo>>
 } {
 	const appContext = useContext(AppContext)
 	const [personalInfo, setPersonalInfo] = useState<BirthDateInfo>({} as BirthDateInfo)
+	const fetchPersonalInfoData = useFetchPersonalInfoData()
+
+	const fetchAndSetPersonalInfo: () => Promise<void> = async () => {
+		try {
+			if (!_.isNull(appContext.sharedData) && !_.isNull(appContext.sharedData.personalInfo)) {
+				setPersonalInfo(appContext.sharedData.personalInfo)
+			}
+			else await fetchPersonalInfoData(setPersonalInfo)
+
+		} catch (error) {
+		}
+	}
 
 	useEffect(() => {
 		if (appContext.auth.userType !== expectedUserType) return
-		const fetchAndSetPersonalInfo: () => Promise<void> = async () => {
-			try {
-				if (!_.isNull(appContext.sharedData) && !_.isNull(appContext.sharedData.personalInfo)) {
-					setPersonalInfo(appContext.sharedData.personalInfo)
-				}
-				else await useFetchPersonalInfoData(setPersonalInfo)
-			} catch (error) {
-			}
-		}
 
 		fetchAndSetPersonalInfo()
-	}, [appContext.auth.userType])
+	}, [appContext.sharedData, appContext.auth.userType])
 
 	return { personalInfo, setPersonalInfo }
 }
